e064625075e2724293e9042a6f2f51788576b12f
Oleggdl <oblotskiyj@gmail.com>
2021-09-15 21:20:11 +0300

    feat: add new feates

---
+++ script.js
@@ -0,0 +1,21 @@
+const scrollToTopButton = document.getElementById("scroll");
+const handleScroll = () => {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    scrollToTopButton.style.display = "block"
+  } 
+  else {
+    scrollToTopButton.style.display = "none";
+  }
+};
+const scrollToTop = () => {
+  const distanceFromTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+  if (distanceFromTop > 0) {
+    window.scrollTo(0, 0);
+  }
+};
+document.addEventListener("scroll", handleScroll);
+scrollToTopButton.addEventListener("click", function(e) {
+  e.preventDefault();
+  scrollToTop();
+});
