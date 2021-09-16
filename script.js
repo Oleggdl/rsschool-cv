ef98b5cc1b50a886eddaadc136a0af7396a665d4
Oleggdl <oblotskiyj@gmail.com>
2021-09-16 13:41:56 +0300

    feat: add burger menu

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const scrollToTopButton = document.getElementById("scroll");
 const handleScroll = () => {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     scrollToTopButton.style.display = "block"
-  } 
+  }
   else {
     scrollToTopButton.style.display = "none";
   }
@@ -15,7 +15,32 @@ const scrollToTop = () => {
   }
 };
 document.addEventListener("scroll", handleScroll);
-scrollToTopButton.addEventListener("click", function(e) {
+scrollToTopButton.addEventListener("click", function (e) {
   e.preventDefault();
   scrollToTop();
 });
+
+
+const burger = document.getElementById('burger');
+const burgerMenu = document.getElementById('burgermenu');
+const headerBorder = document.getElementById('headerMain')
+
+function burgerClick() {
+  
+  
+
+
+}
+burger.onclick = function(){
+  if (burgerMenu.style.display === 'none') {
+    $('#burgermenu').fadeIn();
+    $('#burgerline').fadeIn();
+    // headerBorder.style.borderBottom = '1px rgb(255, 29, 29) solid';
+  } else {
+    $('#burgermenu').fadeOut();
+    $('#burgerline').fadeOut();
+    // headerBorder.style.borderBottom = 'none';
+  }
+  
+  
+};
